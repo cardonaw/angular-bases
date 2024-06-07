68a83b1eff57c2c17a75f486e73843cf7d52c5d7
William A. Cardona <williamxcardona@gmail.com>
2024-06-07 12:14:46 -0400

    adding new features and finishig the section

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,4 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+
+  constructor( private dbzService: DbzService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter( id: string ):void {
+    this.dbzService.deleteCharacterById( id );
+  }
+
+  onNewCharacter( character: Character ) {
+    this.dbzService.addCharacter( character )
+  }
+
 }
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({providedIn: 'root'})
+export class DbzService {
+
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000
+    }, {
+      id: uuid(),
+      name: 'Goku',
+      power: 9500
+    }, {
+      id: uuid(),
+      name: 'Vegeta',
+      power: 7500
+    }, {
+      id: uuid(),
+      name: 'Picoro',
+      power: 500
+    }, {
+      id: uuid(),
+      name: 'Gohan',
+      power: 5000
+    }, {
+      id: uuid(),
+      name: 'Vulma',
+      power: 300
+    }
+  ];
+
+  addCharacter(character: Character):void {
+
+    const newCharacter: Character = { id: uuid(), ...character };
+
+
+    // this.characters.push({ ...character });
+
+    this.characters.push(newCharacter);
+  }
+
+  // onDeleteCharacter(index: number):void {
+    // this.characters.splice(index, 1);
+
+    deleteCharacterById(id: string):void {
+      this.characters = this.characters.filter( character => character.id !== id );
+  }
+
+}
